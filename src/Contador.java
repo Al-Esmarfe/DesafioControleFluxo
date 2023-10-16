54e8c82bca5f734fa093c88ef520004c2b5337d6
Mateus <mateus.alves8@fatec.sp.gov.br>
2023-10-16 18:45:46 -0300

    Adicionando o Desafio de Controle de Fluxo

---
+++ src/Contador.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Contador {
+    public static void main(String[] args) {
+        Scanner terminal = new Scanner(System.in);
+        System.out.println("Digite o primeiro parâmetro");
+        int parametroUm = terminal.nextInt(); // <-- Alteração aqui
+        System.out.println("Digite o segundo parâmetro");
+        int parametroDois = terminal.nextInt(); // <-- Alteração aqui
+
+        try {
+            contar(parametroUm, parametroDois);
+
+        } catch (ParametrosInvalidosException exception) {
+            System.out.println(exception.getMessage());
+        }
+
+    }
+
+    static void contar(int parametroUm, int parametroDois) throws ParametrosInvalidosException {
+        if (parametroUm >= parametroDois) { // <-- Alteração aqui
+            throw new ParametrosInvalidosException("O segundo parâmetro deve ser maior que o primeiro");
+        }
+
+        int contagem = parametroDois - parametroUm;
+        for (int i = 1; i <= contagem; i++) {
+            System.out.println("Imprimindo o número " + i);
+        }
+    }
+}
